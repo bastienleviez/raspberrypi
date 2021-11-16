1458baceb85a60f232219467b4f15265958174ae
Ataz <47629187+bastienleviez@users.noreply.github.com>
2021-11-16 09:21:34 +0100

    pointet nique ta mere

--- Read.cpp
+++ Read.cpp
@@ -1,34 +1,27 @@
 #include "MFRC522.h"
+#include <wiringPi.h>
 #include <stdio.h>
 #include <iostream>
 
+using namespace std;
 int main(){
+  int LED1 = 27;
+  int LED2 = 25; 
   MFRC522 mfrc;
 
   mfrc.PCD_Init();
+  wiringPiSetup();    /* initialize wiringPi setup <wiringPi.h>*/
+  pinMode(LED1,OUTPUT);    /* set GPIO as output <wiringPi.h>*/
+  pinMode(LED2,OUTPUT);
 
   while(1){
-    // Look for a card
-    if(!mfrc.PICC_IsNewCardPresent())
-      continue;
-
-    if( !mfrc.PICC_ReadCardSerial())
-      continue;
-
-    // Print UID
-    for(int i = 0; i < mfrc.uid.size; ++i){
-      if(mfrc.uid.uidByte[i] < 0x10){
-	printf(" 0");
-	printf("%X",mfrc.uid.uidByte[i]);
-      }
-      else{
-	printf(" ");
-	printf("%X", mfrc.uid.uidByte[i]);
-      }
-      
-    }
-    printf("\n");
-    delay(1000);
+    digitalWrite(LED1,HIGH);        /* write high on GPIO <wiringPi.h>*/
+        digitalWrite(LED2, LOW);
+        delay(1000);
+        digitalWrite(LED1, LOW);        /* write low on GPIO */
+        digitalWrite(LED2,HIGH);
+        delay(1000);
   }
   return 0;
 }
+
